4f96e0a4839e33db6cd13089f504edf3457a4b17
Wilson Hernande Neira Mija <wilsonnm22@hotmail.com>
2024-10-07 17:34:25 -0500

    Completado video 84. useEffect

--- git-expert-app/src/GifGrid.jsx
+++ git-expert-app/src/GifGrid.jsx
@@ -1,12 +1,20 @@
 import PropTypes from "prop-types";
 import { getGifs } from "./helpers/getGifs";
+import { useEffect, useState } from "react";
 
 const GifGrid = ({ category }) => {
-  getGifs(category);
+  const [counter, setcounter] = useState(10);
+
+  useEffect(() => {
+    getGifs(category);
+  }, [ ]);
 
   return (
     <>
       <h3> {category} </h3>
+
+      <h5>{counter}</h5>
+      <button onClick={() => setcounter(counter + 1)}> +1 </button>
     </>
   );
 };
