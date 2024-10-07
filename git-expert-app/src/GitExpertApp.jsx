4589ff1adfdc63fc348ec8a4b768bfd69d909dd0
Wilson Hernande Neira Mija <wilsonnm22@hotmail.com>
2024-10-07 16:17:29 -0500

    Completado video 81. validar que sean unicos los nombres

--- git-expert-app/src/GitExpertApp.jsx
+++ git-expert-app/src/GitExpertApp.jsx
@@ -6,7 +6,9 @@ const GitExpertApp = () => {
     const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
 
     const onAddCategory = ( newCategory ) => {
-        console.log(newCategory)
+        
+        if ( categories.includes(newCategory)) return;
+
         setCategories([newCategory, ...categories]);
     };
 
