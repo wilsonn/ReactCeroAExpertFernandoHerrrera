54b61ab99042f8c5acab4d5a5d19489894248733
Wilson Hernande Neira Mija <wilsonnm22@hotmail.com>
2024-10-07 23:13:00 -0500

    Completado video 88. custom hook

--- git-expert-app/src/GifGrid.jsx
+++ git-expert-app/src/GifGrid.jsx
@@ -2,29 +2,19 @@ import PropTypes from "prop-types";
 import { getGifs } from "./helpers/getGifs";
 import { useEffect, useState } from "react";
 import GifItem from "./GifItem";
+import { useFetchGifs } from "./hook/useFetchGifs";
 
 const GifGrid = ({ category }) => {
-  const [images, setImages] = useState([]);
-
-  const getImages = async() => {
-    const newImages = await getGifs(category);
-    setImages(newImages);
-  };
-
-  useEffect(() => {
-    getImages();
-  }, []);
+  const { images, isLoading } = useFetchGifs(category);
 
   return (
     <>
       <h3> {category} </h3>
-      
+
       <div className="card-grid">
-        {
-            images.map((image) => {
-                return <GifItem key={image.id} {...image } />
-            })
-        }
+        {images.map((image) => {
+          return <GifItem key={image.id} {...image} />;
+        })}
       </div>
     </>
   );
