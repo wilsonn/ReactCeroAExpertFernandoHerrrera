f99046c902d7b999bff41e358168071a76818055
Wilson Hernande Neira Mija <wilsonnm22@hotmail.com>
2024-10-07 23:19:22 -0500

    Completado video 89. mostrar mensaje de carga

--- git-expert-app/src/GifGrid.jsx
+++ git-expert-app/src/GifGrid.jsx
@@ -1,6 +1,4 @@
 import PropTypes from "prop-types";
-import { getGifs } from "./helpers/getGifs";
-import { useEffect, useState } from "react";
 import GifItem from "./GifItem";
 import { useFetchGifs } from "./hook/useFetchGifs";
 
@@ -10,7 +8,9 @@ const GifGrid = ({ category }) => {
   return (
     <>
       <h3> {category} </h3>
-
+      {
+        isLoading && (<h2>Cargando...</h2>)
+      }
       <div className="card-grid">
         {images.map((image) => {
           return <GifItem key={image.id} {...image} />;
